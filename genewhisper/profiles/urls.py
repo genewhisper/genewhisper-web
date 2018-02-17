6fc203d01b4743d8d468bcfd26dbf6620a57cfcd
mzhakun <30646730+mzhakun@users.noreply.github.com>
2018-02-16 19:42:08 -0500

    added additional styling

--- genewhisper/profiles/urls.py
+++ genewhisper/profiles/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from profiles import views
-from projects import views as projec_views
+from projects import views as project_views
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('', views.ProfilePage.as_view(template_name='profiles/profile.html'), name='main_profile_page'),
 
-    path('new-project', projec_views.create_new_project, name='new_project_form')
+    path('new-project', project_views.create_new_project, name='new_project_form'),
+    path('project-list', project_views.ProjectList.as_view(template_name='profiles/project_list.html'),
+         name='all_projects_list')
 
 ]
