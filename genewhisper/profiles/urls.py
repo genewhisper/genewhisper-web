376713da640c1e88904b02e5e760dafecbed584d
mzhakun <30646730+mzhakun@users.noreply.github.com>
2018-02-27 02:08:57 -0500

    added report

--- genewhisper/profiles/urls.py
+++ genewhisper/profiles/urls.py
@@ -25,5 +25,10 @@ urlpatterns = [
     path('genomic-profile-delete/<int:pk>', genomic_profile_views.GenomicProfileDeleteView.as_view(),
          name='genomic_profile_delete'),
 
-    path('genomic-profile-report/<int:genomicprofile_id>', pathogenicity_views.report_info, name='report')
+    path('genomic-profile-report/<int:genomicprofile_id>', pathogenicity_views.report_info, name='report'),
+
+    path('genomic-report-list',
+         genomic_profile_views.GenomicProfileListView.as_view(template_name='profiles/genomic_report_list.html'),
+         name='all_genomic_report_list'),
+
 ]
